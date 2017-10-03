8278af4dbba2158f6c915eb26e1b57e814333ea0
ramanohar <ramanohar@users.noreply.github.com>
2017-10-03 11:13:56 +0530

    initial Commit

---
+++ CarlsbergStockPrice/Controllers/StocksController.cs
@@ -0,0 +1,27 @@
+﻿using CarlsbergStockPrice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CarlsbergStockPrice.Controllers
+{
+    public class StocksController : ApiController
+    {
+        Product[] products = new Product[]
+        {
+            new Product { Id = 1, Name = "Carlsberg", Category = "Beer", Price = 200 },
+            
+        };
+
+        [Route("GetStockPrice")]
+        public IEnumerable<Product> GetStockPrice()
+        {
+            return products;
+        }
+
+        
+    }
+}
---
+++ CarlsbergStockPrice/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CarlsbergStockPrice.WebApiApplication" Language="C#" %>
